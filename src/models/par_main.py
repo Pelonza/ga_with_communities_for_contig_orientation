5de804d8a26f8aa0699c03de8b839e6f3c6fd767
Pelonza <karl.schmitt@valpo.edu>
2017-04-14 18:00:44 -0500

    Working parallel code
    Not sure this is giving much speed-up, but it is at least using a few
    processors, instead of one.

---
+++ src/models/par_main.py
@@ -0,0 +1,238 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 14 08:50:14 2017
+
+@author:Karl R. B. Schmitt
+
+
+"""
+from deap import base
+from deap import creator
+from deap import tools
+from deap import algorithms
+
+import random
+import os
+import json
+import multiprocessing
+from scoop import futures
+import dill as pickle
+
+import numpy as np
+import networkx as nx  #Defines G.edges and G.node 
+
+
+#Define an fitness-evaluation function.
+def evaluate(individual, G, Init_pairs):
+    #Inputs are one individual (with array of flip/no-flip orientations), and 
+    # a Graph structure (with nodes/edges from tally file). It returns the 
+    # fitness of the individual.       
+    Orient_pairs=list(Init_pairs)
+    for (u,v,d) in G.edges(data=True):
+        if individual[G.node[u]['idx']]!=individual[G.node[v]['idx']]:
+            Orient_pairs[1]=Orient_pairs[1]-d['w1']+d['w2']
+            Orient_pairs[2]=Orient_pairs[2]-d['w2']+d['w1']
+    
+    return (Orient_pairs[1]/Orient_pairs[0], )
+
+
+#Needs to be outside the function definition so it can be picklable.
+from collections import OrderedDict
+
+class OrderedNodeGraph(nx.Graph):
+    node_dict_factory=OrderedDict
+
+def load_data(input_filename):    
+    #Read in tally file, assuming a 6-column format, no duplicated edges
+    G=nx.read_edgelist(input_filename,nodetype=int, data=(('w1',int),('w2',int),('w3',int),('w4',int)), create_using=OrderedNodeGraph())
+    
+    #Set all edges to 'flipable' and give each node a fixed index for referencing.
+    nx.set_node_attributes(G, 'flippable', True)
+    i=0
+    for n in nx.nodes_iter(G):
+        G.node[n]['idx']=i
+        i=i+1
+    
+    #Compute basic data about the tally file/input data.
+    Initial_matepairs=list([0,0,0]) #(Total, Good, Bad) Total_matepairs=0
+    for (u,v,d) in G.edges(data=True):
+        Initial_matepairs[0]=Initial_matepairs[0]+d['w1']+d['w2']+d['w3']+d['w4']
+        Initial_matepairs[1]=Initial_matepairs[1]+d['w1']
+        Initial_matepairs[2]=Initial_matepairs[2]+d['w2']
+    
+    return G, Initial_matepairs
+
+        
+def is_valid_file(parser, arg):
+    """
+    Check if arg is a valid file that already exists on the file system.
+
+    Parameters
+    ----------
+    parser : argparse object
+    arg : str
+
+    Returns
+    -------
+    arg
+    """
+    arg = os.path.abspath(arg)
+    if not os.path.exists(arg):
+        parser.error("The file %s does not exist!" % arg)
+    else:
+        return arg
+
+
+def get_parser():
+    """Get parser object for script xy.py."""
+    from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+    parser = ArgumentParser(description=__doc__,
+                            formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-i", "--ifile",
+                        dest="ifilename",
+                        type=lambda x: is_valid_file(parser, x),
+                        help="Read tallies from FILE",
+                        metavar="FILE")
+    parser.add_argument("-o", "--ofile",
+                        dest="oorient",
+                        help="Write best orientation to FILE",
+                        metavar="FILE")
+    parser.add_argument("-s", "--sfile",
+                        dest="output_stats",
+                        help="Write statistics to FILE",
+                        metavar="FILE")    
+    parser.add_argument("-n",
+                        dest="n",
+                        default=2,
+                        type=int,
+                        help="how many lines get printed")
+    parser.add_argument("-q", "--quiet",
+                        action="store_false",
+                        dest="verbose",
+                        default=True,
+                        help="don't print status messages to stdout")
+    return parser
+
+
+#These have to be global for multiprocessing.
+creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+creator.create("Individual", list, fitness=creator.FitnessMax)
+
+stats= tools.Statistics(lambda ind: ind.fitness.values)
+stats.register("mean", np.mean)
+stats.register("max", max)
+stats.register("min", min)
+stats.register("std", np.std)
+
+if __name__ == "__main__":
+    """
+    Main that calls a single parameter trial, with multiple runs (args=n)
+    with currently default parameters.
+    """
+    
+    args = get_parser().parse_args()
+    
+    #trial(args)
+
+    params=list([200,1000,0.10,0.20,0.1,0.2])
+    
+    G, Init_mps = load_data(args.ifilename)
+        
+    #Note this is included for readability/reference.
+    #Set some GA parameters:
+    IND_SIZE = nx.number_of_nodes(G)    #Size of each individual in the population
+    POP_SIZE = params[0]                #Size of the overall population
+    NGEN     = params[1]                #Number of generations to evolve for
+    MUT_PB   = params[2]                #Probability that an offspring will mutate
+    CX_PB    = params[3]                #Probability that two children will perform a crossover 
+    MUT_IDPB = params[4]                #Independent probability of an attribute (orientation) mutating
+    CX_IDPB  = params[5]                #Independent probability of an attribute (orientation) crossingover
+    
+    #Define objects for/from DEAP's toolbox
+    #Currently modeled largely off of their one_max example.
+    
+    toolbox=base.Toolbox()
+    toolbox.register("attr_bool", random.randint, 0, 1)
+    toolbox.register("individual",tools.initRepeat,creator.Individual,toolbox.attr_bool, n=IND_SIZE)
+    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+    
+    toolbox.register("evaluate", evaluate, G=G, Init_pairs=Init_mps)
+    toolbox.register("mate", tools.cxUniform, indpb=CX_IDPB)
+    toolbox.register("mutate", tools.mutFlipBit, indpb=MUT_IDPB)
+    toolbox.register("select", tools.selTournament, tournsize=3)
+    
+    #Above could be done outside of "__main__" ??
+    
+    
+    #Below copies DEAP's multiprocessing one-max example inside __main__
+    
+    random.seed(1)
+    
+    pops= [toolbox.population(n=POP_SIZE) for i in range(10)]
+    
+    #pop=toolbox.population(n=POP_SIZE)
+    
+    pool=multiprocessing.Pool()
+    toolbox.register("map",pool.map)
+    
+    #Scoops
+    #toolbox.register("smap", futures.map)
+    
+    toolbox.unregister("attr_bool")
+    toolbox.unregister("individual")
+    toolbox.unregister("population")
+    
+    
+    hof= tools.HallOfFame(1)
+
+    
+    logbook=tools.Logbook()
+    
+    #toolbox.register("algorithm", algorithms.eaSimple, toolbox=toolbox, cxpb=CX_PB, mutpb=MUT_PB, ngen=NGEN, stats=stats, halloffame=hof, verbose=False)
+    
+    
+    for jj in range(10):
+        pop, tlogbook = algorithms.eaSimple(pops[jj],toolbox,cxpb=CX_PB, mutpb=MUT_PB, ngen=NGEN, stats=stats, halloffame=hof, verbose=False)
+        logbook.record(trial=jj, tmax=tlogbook.select('max'))
+    
+    print(logbook)
+    
+    pool.close()
+    
+    
+#    for i in range(args.n):
+#        trialpop, trialstats, trialhof, triallog = ga_func.gaopt_Uni(G, Init_mps, params)
+#        
+#        #print(triallog)
+#        
+#        #This un-packs the logbook generated from the optimization performed 
+#        # above, by keeping each track of information connected to a trial.
+#        logbook.record(trial=i, best=trialhof[0], genmin=triallog.select("min"), 
+#                       genmax=triallog.select("max"), genstd=triallog.select("std"), 
+#                       genavg=triallog.select("mean"), gengen=triallog.select("gen"))
+#        
+##    with open(args.oorient, 'w+') as f:
+##        json.dump(myhof[0], f)
+#    with open(args.output_stats, 'w') as f:
+#        json.dump(logbook,f)
+
+
+
+
+# Mouse line:
+    #-i ..\\..\\data\\raw\\mouse_tallies -o ..\\..\\data\\interim\\mouse.orient -s ..\\..\\data\\interim\\mouse.stat
+    
+        
+        
+        
+        
+        
+
+
+
+
+
+
+
+
+
