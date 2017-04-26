f856b08ee15bc35d6db00f5469daa5c1692eb6ed
Pelonza <karl.schmitt@valpo.edu>
2017-04-26 16:42:41 -0500

    Basic Visualization working for trials
    Uses bokeh to do basic visualizations of time series.

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -12,10 +12,13 @@ Dependencies:
 """
 
 import bokeh as bk
-
+from bokeh.plotting import figure, output_file, show
+from bokeh.layouts import row, column
+import bokeh.palettes as pal
 from deap import tools
 import json
 import os
+import numpy as np
 
 
 def get_parser():
@@ -70,10 +73,74 @@ def is_valid_file(parser, arg):
 if __name__ == "__main__":
     
     
-    args=get_parser().parse_args()
+    args = get_parser().parse_args()
     
-    with open(args.ifilename, 'r') as f:
-        #logbook=json.load(f)
+    mypal = pal.inferno(20)
+    
+    f = open('../../data/interim/mouse_cx.stat','r')
+    M_cx = json.load(f)
+    f.close()
+    
+    f = open('../../data/interim/mouse_cx_comm.stat','r')
+    M_cx_comm = json.load(f)
+    f.close()
+    
+    f = open('../../data/interim/mouse_mutpb.stat','r')
+    M_mut = json.load(f)
+    f.close()
+    
+    f = open('../../data/interim/mouse_mutidpb.stat','r')
+    M_mutid = json.load(f)
+    f.close()
+    
+    
+    output_file("test.html")
+    cxfig=figure()
+    cx_comfig=figure()
+    for j in range(2,19):
+        df = [M_cx[j][k]['tmax'] for k in range(50)]
+        avg_tmax = (np.mean(df, axis=0)).tolist()
+        cxfig.line(M_cx[0][0]['tgen'], avg_tmax, legend="Crossover = "+
+                   str(M_cx[j][0]['tparam'][3]),
+                   line_color=mypal[j], muted_alpha=0.2, alpha=0.8)
+        dfc = [M_cx_comm[j][k]['tmax'] for k in range(50)]
+        avg_tmaxc = (np.mean(dfc, axis=0)).tolist()
+        cx_comfig.line(M_cx_comm[0][0]['tgen'], avg_tmaxc, 
+                       legend="Crossover = "+str(M_cx[j][0]['tparam'][3]),
+                       line_color=mypal[j], muted_alpha=0.2, alpha=0.8)
+    
+    cxfig.legend.location = "bottom_right"
+    cxfig.legend.click_policy = "hide"
+    
+    cx_comfig.legend.location = "bottom_right"
+    cx_comfig.legend.click_policy = "hide"
+    
+    mutidfig = figure()
+    mutid_comfig = figure()
+    for j in range(10):
+        df = [M_mutid[j][k]['tmax'] for k in range(50)]
+        avg_tmax = (np.mean(df, axis=0)).tolist()
+        mutidfig.line(M_mutid[0][0]['tgen'], avg_tmax, 
+                      legend="Ind. Mut. = "+str(M_mutid[j][0]['tparam'][4]),
+                      line_color=mypal[j])
         
     
-    print(logbook)
+    mutidfig.legend.location = "bottom_right"
+    mutidfig.legend.click_policy = "hide"
+    
+    show(column(cxfig,cx_comfig, mutidfig) )  
+
+    
+    
+    """
+    Process:
+        
+        load logbook
+        df = [ logbook[9][k]['tmax'] for k in range(50)] #Generate sub-data frame
+        avg_tmax=np.mean(df, axis=0)  # Average Data
+        avg_tmax_lst=avg_tmax.tolist()  #  Un-array the averages
+        plot! : p.line(logbook[x][0]['tgen'], avg_tmax_lst)
+        
+        
+        average tmax: avg_max=numpy.mean()
+    """
